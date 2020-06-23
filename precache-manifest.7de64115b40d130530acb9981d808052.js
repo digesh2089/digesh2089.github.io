2af257a1ce7553519437d82ef1c03a4bbae81d5b
unknown <digesh2089@gmail.com>
2020-06-23 12:08:50 +0530

    deploy

---
+++ precache-manifest.7de64115b40d130530acb9981d808052.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70d29880861903ecd41b",
+    "url": "/css/app.0e898d85.css"
+  },
+  {
+    "revision": "44d36813cea0caa9bcf5",
+    "url": "/css/chunk-vendors.856dd0d3.css"
+  },
+  {
+    "revision": "33c2d85a25ce0bb133b498289aa24993",
+    "url": "/img/Baguette.33c2d85a.png"
+  },
+  {
+    "revision": "e314ff026412e4bf91a8a902b9432914",
+    "url": "/img/CUSHION.e314ff02.png"
+  },
+  {
+    "revision": "3aaeceeda7f9198b600c140179d703c9",
+    "url": "/img/EMERALD.3aaeceed.png"
+  },
+  {
+    "revision": "d6535e3fcf54c7fb5536fd500f5a5bd8",
+    "url": "/img/MARQUISE.d6535e3f.png"
+  },
+  {
+    "revision": "bb647a90f270ae635d0da872022da770",
+    "url": "/img/PEAR.bb647a90.png"
+  },
+  {
+    "revision": "fbb505a59ae0965bec625187a2add7af",
+    "url": "/img/PRINCESS.fbb505a5.png"
+  },
+  {
+    "revision": "7e79165aa7a338921217cef9c6095eb6",
+    "url": "/img/ROUND.7e79165a.png"
+  },
+  {
+    "revision": "599f26b3c7348a8f4182c6b1dda45ab6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "34aacf20902147a957ad",
+    "url": "/js/about.c95852a5.js"
+  },
+  {
+    "revision": "70d29880861903ecd41b",
+    "url": "/js/app.359c1647.js"
+  },
+  {
+    "revision": "44d36813cea0caa9bcf5",
+    "url": "/js/chunk-vendors.cb1cc911.js"
+  },
+  {
+    "revision": "37b3824fe4783dc3b068be4405f6528b",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
